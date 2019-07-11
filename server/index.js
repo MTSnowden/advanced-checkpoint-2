c4b6a2457b43add9280740e5f16b6fe1e2d80a64
Michael Snowden <mtsnowden@gmail.com>
2019-07-11 15:49:49 -0500

    working

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from './express/routes'
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
-// const cors = require('cors');
+
 
 
 mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
+
 mongoose.connect("mongodb+srv://Oprah:Oprah@cluster0-chqnf.mongodb.net/test?retryWrites=true&w=majority");
 
 app.use(router)
-// app.use(cors())
+app.use(express.statuc("public"))
+app.use(bodyParser.json())
+
 
 const port = process.env.PORT || 3105;
 app.listen(port, () => {
